1ce55cfd844813441a783b687dee47c7f9049d3d
GatlingGoBrrr <68349783+GatlingGoBrrr@users.noreply.github.com>
2024-11-23 23:14:00 -0800

    Update ElementCombiner.cs

--- MagicalMaterialMixing/Assets/ElementCombiner.cs
+++ MagicalMaterialMixing/Assets/ElementCombiner.cs
@@ -7,27 +7,16 @@ using UnityEngine;
 public class ElementCombiner : MonoBehaviour
 {
     public GameObject newElement;
-
-    private void Start()
-    {
-        newElement.GetComponent<SpriteRenderer>().enabled = false;
-    }
     
     public void CombineElements(GameObject element1, GameObject element2)
     {
-        Destroy(element1);
-        Destroy(element2);
-
-        if ((element1.name == "Haroonium" && element2.name == "Declanite") || (element1.name == "Declanite" && element2.name == "Haroonium"))
+        if((element1.name == "Haroonium" && element2.name == "Declanite") || (element1.name == "Declanite" && element2.name == "Haroonium"))
         {
+            Destroy(element1);
+            Destroy(element2);
             Instantiate(newElement, new Vector3(0, 0, 0), transform.rotation);
-            newElement.GetComponent<SpriteRenderer>().enabled = true;
         }
-        else
-        {
-            Debug.Log("poof");
-        }
-    
-    
+
+        
     }
 }
