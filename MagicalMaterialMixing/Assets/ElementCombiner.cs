05c71b8376ebeed4802981d6dd72f11555d2fe52
GatlingGoBrrr <68349783+GatlingGoBrrr@users.noreply.github.com>
2024-11-23 19:13:20 -0800

    Establish link between Logic Manager and Collision Detector

---
+++ MagicalMaterialMixing/Assets/ElementCombiner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ElementCombiner : MonoBehaviour
+{
+    public void CombineElements(GameObject element1, GameObject element2)
+    {
+        Debug.Log(element1.name + " + " + element2.name + " = I don't know yet");
+    }
+}
